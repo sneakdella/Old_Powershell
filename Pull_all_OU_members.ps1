ca4a393e75c4e67ab5f37c348b6f5415f4c94df7
Yuhasz <yuhas.jake@gmail.com>
2018-09-01 22:18:09 -0700

    Init
    Me scripts

---
+++ Pull_all_OU_members.ps1
@@ -0,0 +1,28 @@
+﻿<# WRITTEN BY JACOB YUHAS #>
+
+<# This script will #>
+<# Retrieve all Security Groups of a specific OU #>
+<# Then list all the group members of each Security Group #>
+<# Then it writes it to a TXT file in ASCII #>
+<# ---------------------------------------------------------#>
+<# The main intent of this script is to grab security groups#>
+<# Then create a file for each security group and place the #>
+<# members of the group into that text file, so that it can #>
+<# be imported into Remote Desktop Connection Manager 2.7!  #>
+$ou_list = @()
+
+Get-ADGroup -Filter 'Name -like "*"' -SearchBase "OU=ComputerGroups,DC=jakeyuhas,DC=com" | select name | ForEach-Object {$ou_list += $_}
+
+ForEach ($grp in $ou_list) {
+    $filename = $grp.name
+    Write-Host $grp.name
+    Write-Host "--------"
+    $child_list = @()
+    Get-ADGroupMember -Identity $grp.name | select name | ForEach-Object {$child_list += $_}
+    ForEach ($child in $child_list) {
+        $lechild = $child.name
+        Write-Host "$lechild"
+        "$lechild" | Out-File -append -Encoding Ascii C:\Users\Administrator.JAKEYUHAS\Desktop\$filename.txt
+    }
+    Write-Host ""
+}
